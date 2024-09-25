52e534dbaabcd1c8b23ba87a30446396b68c3590
zhen1007 <zhen1007@outlook.com>
2024-09-25 05:45:29 -0400

    task 7

--- server.py
+++ server.py
@@ -8,11 +8,14 @@ def emo_detector():
     text_to_analyze = request.args.get('textToAnalyze')
     result = emotion_detector(text_to_analyze)
 
+    if not result["dominant_emotion"]:
+        return "Invalid text! Please try again!"
+
     return f"For the given statement, the system response is 'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']} and 'sadness': {result['sadness']}. The dominant emotion is {result['dominant_emotion']}."
     
 @app.route("/")
 def render_index_page():
     return render_template('index.html')
-    
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
