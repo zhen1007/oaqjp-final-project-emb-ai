1730e61cd23baac00fc0b9d0224a9dba9a2959a1
zhen1007 <zhen1007@outlook.com>
2024-09-25 05:48:52 -0400

    task 8

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+''' Module docstring is to be here '''
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,16 +6,23 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector", methods=['GET', 'POST'])
 def emo_detector():
+    ''' Function docstring for emotionDetector is to be here '''
     text_to_analyze = request.args.get('textToAnalyze')
     result = emotion_detector(text_to_analyze)
 
     if not result["dominant_emotion"]:
         return "Invalid text! Please try again!"
 
-    return f"For the given statement, the system response is 'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']} and 'sadness': {result['sadness']}. The dominant emotion is {result['dominant_emotion']}."
-    
+    return (
+        f"For the given statement, the system response is "
+        f"'anger': {result['anger']}, 'disgust': {result['disgust']}, "
+        f"'fear': {result['fear']}, 'joy': {result['joy']} and "
+        f"'sadness': {result['sadness']}. The dominant emotion is {result['dominant_emotion']}."
+    )
+
 @app.route("/")
 def render_index_page():
+    ''' Function docstring for / is to be here '''
     return render_template('index.html')
 
 if __name__ == '__main__':
