ce37d74b3871677544f03a11fddd5df5162c7125
zhen1007 <zhen1007@outlook.com>
2024-09-25 05:26:01 -0400

    task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -14,8 +14,36 @@ def emotion_detector(text_to_analyze):
     try:
         # Make a POST request to the Emotion Detection service
         response = requests.post(url, headers=headers, json=input_json)
-        return response.text
 
+        # Check the response
+        if response.status_code == 200:
+            # Parse the response JSON and extract emotion scores
+            formatted_response = json.loads(response.text)
+            emotion_predictions = formatted_response.get('emotionPredictions', [])
+            
+            # Extract required emotions and their scores
+            emotions = emotion_predictions[0].get('emotion', {})
+            # Find the dominant emotion
+            dominant_emotion = max(emotions, key=emotions.get)
+            
+            # Add the dominant emotion to emotions
+            formatted_emotion = {**emotions, 'dominant_emotion': dominant_emotion}
+            return formatted_emotion
+            
+        elif response.status_code == 400:
+            # Handle blank entry error
+            return {
+                'anger': None,
+                'disgust': None,
+                'fear': None,
+                'joy': None,
+                'sadness': None,
+                'dominant_emotion': None
+            }
+        else:
+            print(f"Error: Emotion Detection request failed with status code {response.status_code}")
+            return None
+            
     except Exception as e:
         print(f"Error: An exception occurred in case you experience connection error - {e}")
         return None
